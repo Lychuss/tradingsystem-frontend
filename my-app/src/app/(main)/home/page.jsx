07aacc07a789b652375c88d09a4ac05b0270cb85
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-10-05 01:52:30 +0800

    move the home page

--- my-app/src/app/(main)/home/page.jsx
+++ my-app/src/app/(main)/home/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Books from '../(components)/Home.jsx';
+import Books from '../../(components)/Home.jsx';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation.js';
 
