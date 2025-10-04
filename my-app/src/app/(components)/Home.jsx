300f6b773a17214f14c08f3169ab48cdd3719e85
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-10-04 17:56:22 +0800

    fix how the cards display make it more responsive

--- my-app/src/app/(components)/Home.jsx
+++ my-app/src/app/(components)/Home.jsx
@@ -8,8 +8,13 @@ export default function Books(){
     const [items, setItems] = useState([]);
 
     const methods = (method, type1, type2) => {
-        if(method === 2) return <p>Price: ₱{type2}</p>;
-        return <p>Requirements: {type1}</p>
+        if(method === 1 ){
+            if(type1.length > 10) {
+                return <p>Requirements: {type1.slice(0, 10) + '...'}</p>
+            } 
+            return <p>Requirements: {type1}</p>
+        }
+        return <p>Price: ₱{type2}</p>;
     }
 
     const router = useRouter();
@@ -45,21 +50,35 @@ export default function Books(){
     console.log(items);
 
      return (
-        <>
-          <div className="inline-block flex px-12">
-                {items.map((item) => 
-                (<section className="inline-block flex space-x-10 p-2 m-6" key={item.product_id}>
-                    <div>
-                        <img className="w-60 h-60 rounded-3xl" src={item.url} alt={item.title}></img>
-                          <div className="font-mono m-2">
-                            <h1 className="font-bold ">{item.title}</h1>
+                <>
+                <div className="flex flex-wrap">
+                    {items.map((item) => (
+                    <section
+                        key={item.product_id}
+                        className="flex flex-col p-2 my-6 space-x-10 w-83 items-center"
+                    >
+                        <div>
+                        <img
+                            className="w-70 h-70 rounded-3xl object-cover"
+                            src={item.url}
+                            alt={item.title}
+                        />
+                        <div className="font-mono m-2 flex inline-block">
+                            <h1 className="font-bold">{item.title}</h1>
                             {methods(item.methods, item.requirements, item.price)}
-                            <button className="font-bold mx-auto block border 
-                            border-black rounded-lg px-4 py-2 my-2 bg-green-700 cursor-pointer">Trade</button>
-                          </div>
-                    </div>
-                </section>))}
-          </div>  
-        </>
+                        </div>
+                            <button
+                            className="font-bold border border-black rounded-lg px-4 py-2 my-2 bg-green-700 
+                            cursor-pointer mx-auto block"
+                            >
+                            Trade
+                            </button>
+                        </div>
+                    </section>
+                    ))}
+                </div>
+                </>
+
     );
 }
+
