7d9fec5600db91aa728cff9a7cac42ff9d040d12
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-10-09 00:19:10 +0800

    create a request fetch for getting a specific product

---
+++ my-app/src/app/(main)/products/[id]/page.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+const token = localStorage.getItem('token').token ? JSON.parse(localStorage.getItem('token')).token : null;
+
+export default function Product({ params }){
+    const { id } = params;
+
+    const getProduct = async () =>{
+
+        const response = await fetch(`${process.env.NEXT_PUBLIC_LOCAL_API_URL}/yes4trade/{${id}}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+    } 
+}
