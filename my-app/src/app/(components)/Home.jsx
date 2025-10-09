fb776d7adc3be936f5d747721f2934cabc8be5b0
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-10-10 00:18:51 +0800

    created an onclick for getting product

--- my-app/src/app/(components)/Home.jsx
+++ my-app/src/app/(components)/Home.jsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation.js";
 
 export default function Books(){
 
+
     const [items, setItems] = useState([]);
 
     const methods = (method, type1, type2) => {
@@ -70,7 +71,7 @@ export default function Books(){
                             <h1 className="font-bold">{item.title}</h1>
                             {methods(item.methods, item.requirements, item.price)}
                         </div>
-                            <button
+                            <button onClick={() => router.push(`/product/${item.product_id}`)}
                             className="font-bold border border-black rounded-lg px-4 py-2 my-2 bg-green-700 
                             cursor-pointer mx-auto block"
                             >
