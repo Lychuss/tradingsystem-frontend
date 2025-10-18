041b88d732bfa92f1c35d630f421ef7e4660e70f
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-10-19 00:28:49 +0800

    created pages and endpoints for notes and uniforms

--- my-app/src/app/(main)/home/products/notes/page.jsx
+++ my-app/src/app/(main)/home/products/notes/page.jsx
@@ -22,7 +22,7 @@ export default function BooksProduct(){
         if(!token) return;
 
        const fetchBook = async () => { 
-        const response = await fetch(`${process.env.NEXT_PUBLIC_LOCAL_API_URL}/yes4trade/filter/books/products`, {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_LOCAL_API_URL}/yes4trade/filter/notes/products`, {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${token}`
