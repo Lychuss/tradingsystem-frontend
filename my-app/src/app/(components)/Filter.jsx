ec4b59ee697288656c3e04acd27c7b28ae0797f2
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-10-20 00:40:27 +0800

    created a dropmenu in profile

--- my-app/src/app/(components)/Filter.jsx
+++ my-app/src/app/(components)/Filter.jsx
@@ -14,7 +14,7 @@ export default function Filter(){
         }
     }
 
-    return  <select onChange={handlePage} className="text-4 border rounded-lg border-white py-1 px-5">
+    return  <select onChange={handlePage} className="text-4 border rounded-lg border-white py-1 px-5 cursor-pointer">
               <option value="" className="bg-black">SELECT</option>
               <option value="Trade" className="bg-black">TRADE</option>
               <option value="Buy" className="bg-black">BUY</option>
--- my-app/src/app/(components)/Header.jsx
+++ my-app/src/app/(components)/Header.jsx
@@ -2,14 +2,28 @@
 
 import { FaShoppingCart } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
+import { useState } from "react";
 import Text from "./Text";
 
 export default function Header() {
+  
+  const [show, setShow] = useState(false);
+
   return (
     <>
       <FaShoppingCart className="inline-block border rounded-sm text-black mr-31 w-10 h-10 p-2 bg-white" />
       <Text />
-      <CgProfile className="inline-block rounded-sm text-white w-13 h-13 p-2" />
+      <CgProfile onClick={() => setShow(!show)} className="inline-block cursor-pointer rounded-sm text-white w-13 h-13 p-2" />
+      {show && (        
+        <div className="absolute z-50 bg-gray-800 w-40 h-40 right-15 rounded-lg">
+            <ul>
+                <li className="border-b border-black p-2 hover:rounded-md hover:bg-gray-700 cursor-pointer transition">Profile</li>
+                <li className="border-black border-b p-2 hover:shadow-md hover:bg-gray-700 cursor-pointer transition">Trade</li>
+                <li className="border-black border-b p-2 hover:shadow-md hover:bg-gray-700 cursor-pointer transition">About</li>
+                <li className="border-black border-b p-2 hover:rounded-md hover:bg-gray-700 cursor-pointer transition">Settings</li>
+            </ul>
+        </div>
+    )}
     </>
   );
 }
